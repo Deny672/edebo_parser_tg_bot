bf46152ed7e0a8af2195d3a34c3334bf1e1c16fa
Denys <denis06699@gmail.com>
2023-07-20 12:23:17 +0300

    Add files via upload

---
+++ gitignore.py
@@ -0,0 +1,27 @@
+token=('6335625749:AAG8LbqMk5SykaEuvr2wqvNVIfFqMs1xctc')
+cookies = {
+        '_ga': 'GA1.1.870051912.1687180766',
+        'PHPSESSID': 'ka9rrac0cb66o70makeiftnqpu',
+        '_ga_WLDPX624RK': 'GS1.1.1689312206.6.0.1689312207.0.0.0',
+        '_ga_W6WT1K3VXZ': 'GS1.1.1689509036.4.1.1689509594.0.0.0',
+        '_ga_YC32TV7WL7': 'GS1.1.1689772565.1.1.1689772576.0.0.0',
+        '_ga_96Q7K30V0N': 'GS1.1.1689790129.11.1.1689790311.0.0.0',
+    }
+id_chat_message = ''
+headers = {
+        'Accept': 'application/json, text/javascript, */*; q=0.01',
+        'Accept-Language': 'ru-RU,ru;q=0.9,en-US;q=0.8,en;q=0.7',
+        'Connection': 'keep-alive',
+        'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8',
+        # 'Cookie': '_ga=GA1.1.870051912.1687180766; PHPSESSID=ka9rrac0cb66o70makeiftnqpu; _ga_WLDPX624RK=GS1.1.1689312206.6.0.1689312207.0.0.0; _ga_W6WT1K3VXZ=GS1.1.1689509036.4.1.1689509594.0.0.0; _ga_YC32TV7WL7=GS1.1.1689772565.1.1.1689772576.0.0.0; _ga_96Q7K30V0N=GS1.1.1689790129.11.1.1689790311.0.0.0',
+        'Origin': 'https://vstup.edbo.gov.ua',
+        'Referer': f'https://vstup.edbo.gov.ua/offer/{id_chat_message}/',
+        'Sec-Fetch-Dest': 'empty',
+        'Sec-Fetch-Mode': 'cors',
+        'Sec-Fetch-Site': 'same-origin',
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/113.0.0.0 Safari/537.36 OPR/99.0.0.0',
+        'X-Requested-With': 'XMLHttpRequest',
+        'sec-ch-ua': '"Opera";v="99", "Chromium";v="113", "Not-A.Brand";v="24"',
+        'sec-ch-ua-mobile': '?0',
+        'sec-ch-ua-platform': '"Windows"',
+    }
